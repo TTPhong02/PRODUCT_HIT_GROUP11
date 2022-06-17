efe163c7783e97f8e8cb418456be5a44fe627e37
ThoNguyen <hatigr.th@gmail.com>
2022-06-17 12:43:29 +0700

    update

--- myweb/src/pages/ProductView/ProductView.jsx
+++ myweb/src/pages/ProductView/ProductView.jsx
@@ -6,7 +6,50 @@ import ViewImagesProduct from "../../components/ViewImagesProduct/ViewImagesProd
 import ProductReviews from "../../components/ProductReviews/ProductReviews";
 import AdditionalInformation from "../../components/AdditionialInfomation/AdditionalInformation";
 import UserReview from "../../components/UserReview/UserReview";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import WriteReviews from "../../components/WriteReviews/WriteReviews";
+import Product from "../../components/Product/index";
 const ProductView = () => {
+  const productsHot = [
+    {
+      id: 1,
+      sale: "50",
+      src: "https://congtyvanhanhtoanha.com/wp-content/uploads/2018/09/slide2-cong-ty-van-hanh-toa-nha-ha-noi-2.jpg",
+      name: "Giày Thể Thao Nam Bitis DSM074933XAM (Xám) - Giày chính hãng",
+      price: "500.000",
+    },
+
+    {
+      id: 2,
+      src: "https://www.mpe.com.vn/Data/Sites/1/News/105/150223eye330swacker1jpgcroporiginal-original-1507629292709.jpg",
+      name: "Giày Thể Thao Nam Bitis DSM074933XAM (Xám) - Giày chính hãng",
+      price: "500.000",
+    },
+
+    {
+      id: 3,
+      sale: "50",
+      src: "https://congtyvanhanhtoanha.com/wp-content/uploads/2018/09/slide2-cong-ty-van-hanh-toa-nha-ha-noi-2.jpg",
+      name: "Giày Thể Thao Nam Bitis DSM074933XAM (Xám) - Giày chính hãng",
+      price: "500.000",
+    },
+
+    {
+      id: 4,
+      src: "https://congtyvanhanhtoanha.com/wp-content/uploads/2018/09/slide2-cong-ty-van-hanh-toa-nha-ha-noi-2.jpg",
+      name: "Giày Thể Thao Nam Bitis DSM074933XAM (Xám) - Giày chính hãng",
+      price: "500.000",
+    },
+
+    {
+      id: 5,
+      src: "https://congtyvanhanhtoanha.com/wp-content/uploads/2018/09/slide2-cong-ty-van-hanh-toa-nha-ha-noi-2.jpg",
+      name: "Giày Thể Thao Nam Bitis DSM074933XAM (Xám) - Giày chính hãng",
+      price: "500.000",
+    },
+  ];
+
   return (
     <div className="contain-page">
       <div className="info-product">
@@ -25,10 +68,35 @@ const ProductView = () => {
         <UserReview />
         <UserReview />
         <UserReview />
-<<<<<<< HEAD
         <UserReview />
-=======
->>>>>>> 8e2637e (update code view product...)
+
+        <div className="btn-view__more">
+          <div className="btn-more btn-move">
+            <FontAwesomeIcon icon={faAngleLeft} />
+          </div>
+          <div className="btn-more active">1</div>
+          <div className="btn-more">2</div>
+          <div className="btn-more">3</div>
+          <div className="btn-more">4</div>
+          <div className="btn-more">5</div>
+          <div className="btn-more">...</div>
+          <div className="btn-more btn-move">
+            <FontAwesomeIcon icon={faAngleRight} />
+          </div>
+        </div>
+      </div>
+      <WriteReviews />
+      <p className="title-products">Sản phẩm tương tự</p>
+      <div className="products-list grid lg:grid-cols-5 lg:grid-rows-1 md:grid-cols-2 sm:grid-cols-2">
+        {productsHot.map((item) => (
+          <Product
+            key={item.id.toString()}
+            src={item.src}
+            name={item.name}
+            price={item.price}
+            sale={item.sale}
+          />
+        ))}
       </div>
       <Brands />
     </div>
