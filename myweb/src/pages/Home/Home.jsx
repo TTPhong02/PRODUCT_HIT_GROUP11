527e84ed347c28cfdf8fae7138c573bba91804a9
TTPhong02 <Phong762002@gmail.com>
2022-08-05 05:50:22 +0700

    update login

--- myweb/src/pages/Home/Home.jsx
+++ myweb/src/pages/Home/Home.jsx
@@ -7,16 +7,18 @@ import ProductsHot from "../../components/ProductsHot/ProductsHot";
 import ProductsNew from "../../components/ProductsNew/ProductsNew";
 import News from "../../components/News/News";
 import Brands from "../../components/Brands/Brands";
+import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getAllUser, getCartFromUSer } from "../../redux/apiRequest/apiRequest";
-import { useDispatch } from "react-redux";
 
 const Home = () => {
   const dispatch = useDispatch()
+
   useEffect(()=>{
     getAllUser(dispatch)
     getCartFromUSer(dispatch)
   },[])
+
   return (
     <div className="container ">
       <SliderBar />
