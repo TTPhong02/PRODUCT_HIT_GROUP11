008362792222d21d468043fc724c957245280ce7
ThoNguyen <hatigr.th@gmail.com>
2022-08-07 03:11:38 +0700

    update

--- myweb/src/routers/Routers.jsx
+++ myweb/src/routers/Routers.jsx
@@ -16,8 +16,9 @@ import { NewFeed } from "../pages/NewFeed/NewFeeds/NewFeeds";
 import ProductManagement from "../pages/Admin/ProductManagement/ProductManagement";
 import AccountManagement from "../pages/Admin/AccountManagement/AccountManagement";
 import InforFeeds from "../pages/NewFeed/InforFeeds/InforFeeds";
-import EventManagement from "../pages/Admin/EventManagement/EventManagement"
-import VoucherManagement from "../pages/Admin/VoucherManagement/VoucherManagement"
+import EventManagement from "../pages/Admin/EventManagement/EventManagement";
+import VoucherManagement from "../pages/Admin/VoucherManagement/VoucherManagement";
+import ListVoucher from "../pages/ListVourcher/ListVoucher";
 
 const Routers = () => {
   return (
@@ -38,8 +39,9 @@ const Routers = () => {
       <Route path="/paysuccess" element={<PayDone />} />
       <Route path="/admin/account" element={<AccountManagement />} />
       <Route path="/admin/product" element={<ProductManagement />} />
-      <Route path="/admin/event" element={<EventManagement/> } />
-      <Route path="/admin/voucher" element={<VoucherManagement/>}/>
+      <Route path="/admin/event" element={<EventManagement />} />
+      <Route path="/admin/voucher" element={<VoucherManagement />} />
+      <Route path="/listVoucher" element={<ListVoucher />} />
     </Routes>
   );
 };
