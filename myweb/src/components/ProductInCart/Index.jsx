d29d41ed39037dc1212a9022bb1f4b59a6c212bd
TTPhong02 <Phong762002@gmail.com>
2022-08-07 10:18:13 +0700

    upadte

--- myweb/src/components/ProductInCart/Index.jsx
+++ myweb/src/components/ProductInCart/Index.jsx
@@ -13,7 +13,7 @@ export const ProductInCart = (props) => {
   return (
     <div className="product">
         <div className="product_image">
-            <img src={numberWithCommas(item.src)} alt="" />
+            <img src={item.src} alt="" />
         </div>
         <div className="product_infor">
             <p className='product_infor-name pr-2 pb-2'>{item.title}</p>
@@ -21,7 +21,7 @@ export const ProductInCart = (props) => {
             <p className='product_infor-size pr-2 pb-2'>Kích Cỡ: {item.size}</p>
             <div className="product_infor-sumprice flex pb-5">
                 <p className='pr-2 '>{item.quantity} x </p>
-                <p className='pr-2 - text-red-500 font-bold'>{item.price} VND</p>
+                <p className='pr-2 - text-red-500 font-bold'>{numberWithCommas(item.price)} VND</p>
             </div>
         </div>
     </div>
